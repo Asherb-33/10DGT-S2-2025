da45a10a81b178545fb18a868ad345df4609f10c
Asherb-33 <asher.brown@kingsway.school.nz>
2025-10-08 14:24:22 +1300

    My python files that I created in class

---
+++ Variables.py
@@ -0,0 +1,13 @@
+#demonstrate how variable are created and how they work
+#Author: Asher Brown
+#Date: 17-09-25
+#v1.0
+
+#Different types pf variables
+#store a string
+greeting="Hello World!"
+print (greeting)
+
+num_1=7
+num_1+1
+print (num_1)
---
+++ animal_data.py
@@ -0,0 +1,12 @@
+#Python code: Name of the file should be animal_data.py
+#date:19-09-25
+#Author: Asher Brown
+animal_name ="Python"
+amount_of_legs =0
+animal_length=5
+animal_tail=False
+
+#displaying the animal data to the user
+print(f"The{animal_name }has {amount_of_legs} legs.")
+print(f"The{animal_name} is {animal_length}m long.")
+print(f"The {animal_name} has a tail:{animal_tail} ")
